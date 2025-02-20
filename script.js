8ffd26bc5c81b0b94983d8eb1cfcf000e575bc98
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-02-20 09:53:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function convenio() {
+    location.href = 'index2.html';
+}
+
+function a() {
+    location.href = 'https://docs.google.com/spreadsheets/d/1AqDa_jx5DmvPxmmPRZ8BFpVfuqbOKBEiAcE8MGBmLnA/edit?usp=drive_link';
+}
+
+function b() {
+    location.href = 'https://docs.google.com/spreadsheets/d/1yt_qeXiGPjnSmJKmJsifPMFRVVQyPqfp/edit?usp=drive_link&ouid=102177858714136949873&rtpof=true&sd=true';
+}
+
+function c() {
+    location.href = 'index.html';
+}
