3bcf972a917128b8a00dbea2bf1f32be972060d8
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-03-28 16:47:31 -0300

    Update script.js
    mudança de código

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-function convenio() {
-    location.href = 'index2.html';
-}
-
-function a() {
-    location.href = ' ';
-}
-
-function b() {
-    location.href = ' ';
-}
-
 function c() {
     location.href = 'index.html';
+}
+function convenio() {
+    location.href = 'index2.html';
 }
 function cst() {
     location.href = 'index3.html'
