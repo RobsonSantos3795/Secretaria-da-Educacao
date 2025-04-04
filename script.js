b9728af9b465b889939031b6aeb89a59b7652e7a
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2025-04-04 13:52:21 -0300

    Update script.js
    Modificação de script

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function c() {
-    location.href = 'index.html';
-}
 function convenio() {
     location.href = 'index2.html';
 }
+
+function a() {
+    location.href = '';
+}
+
+function b() {
+    location.href = '';
+}
+
+function c() {
+    location.href = 'index.html';
+}
+// TESTE SHEETS
+
 function cst() {
-    location.href = 'index3.html'
+    location.href = 'index10.html';
 }
